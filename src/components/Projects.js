9e734f92e16752cb2dab3805fb8fcad2cfea7b7f
HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
2025-05-24 14:27:18 -0400

    attempting to add pdf

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -353,6 +353,11 @@ function Projects() {
             <Modal.Title>myFlix React Client Case Study</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            <object data="/case-studies/myFlixCaseStudy.pdf" type="image/jpg" width="100%" height="600px">
+            <p className="text-center text-secondary">
+              If the PDF does not load, you can download it{" "}
+              </p>
+            </object>
             <h4>Project Overview</h4>
             <p className="text-primary mb-4">
               myFlix is a full-stack movie database app that empowers users to
