99bff885d9114d6593c5e03ca8b105b373eab4a8
HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
2025-06-01 16:23:24 -0400

    Squashed commit of the following:
    commit 0f0856e08bee98acd5741dc9064a6cc01d0f3917
    Author: HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
    Date:   Sun Jun 1 16:23:04 2025 -0400
    
        update README
    
    commit ce23ed9d13a1f8369f55762e3a2aa267f707c904
    Author: HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
    Date:   Sun Jun 1 15:53:15 2025 -0400
    
        Adding case study and tweaking pictures
    
    commit 9fb3a4bcafd7d62fc27cb251d5c82c1b293e5a8a
    Author: HIPgirl95 <162224454+HIPgirl95@users.noreply.github.com>
    Date:   Sun Jun 1 14:37:21 2025 -0400
    
        Create Mobile Case Study Modal

--- README.md
+++ README.md
@@ -1,70 +1,43 @@
-# Getting Started with Create React App
+# Developer Portfolio
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Welcome! This is my personal portfolio built with **React** and **JavaScript**, designed to showcase my work as a full-stack developer. It highlights selected projects, technical skills, and a bit about who I am as a developer and problem-solver.
 
-## Available Scripts
+## 🚀 Features
 
-In the project directory, you can run:
+- Responsive, mobile-friendly design
+- Dark mode toggle for better accessibility
+- Smooth navigation with React Router
+- Project gallery with live demos and GitHub links
+- About section with background and interests
+- Contact section for easy connection
 
-### `npm start`
+## 🛠️ Tech Stack
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+- **Frontend**: React, JavaScript, HTML5, CSS3
+- **Tooling**: Create React App
+- **Styling**: Custom CSS
+- **Deployment**: Vercel
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## 🌐 Live Site
 
-### `npm test`
+[View Portfolio on Vercel](https://hannah-hogan-portfolio.vercel.app/)
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## 📸 Preview
 
-### `npm run build`
+<!-- You can add a screenshot or GIF below -->
+![Portfolio Screenshot](/public/images/Portfolio.png)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## ✍️ About Me
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+I'm a full-stack developer with a background in physics education and a passion for solving problems with code. This portfolio is a creative project and living resume, built to reflect my skills, interests, and growth as a developer.
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## 📫 Contact
 
-### `npm run eject`
+You can reach me via:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+- [LinkedIn](https://www.linkedin.com/in/hannah-i-hogan/)
+- Or through the contact section on the site
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+---
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Thanks for visiting!
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,6 +19,7 @@ function Projects() {
   const handleClose = () => setShowCaseStudy(null);
   const handleShowReactCaseStudy = () => setShowCaseStudy("react");
   const handleShowAngularCaseStudy = () => setShowCaseStudy("angular");
+  const handleShowMobileCaseStudy = () => setShowCaseStudy("mobile");
 
   return (
     <section id="projects" className="projects-section py-5">
@@ -224,6 +225,11 @@ function Projects() {
           {/* Project 5 */}
           <Col lg={4} className="mb-4">
             <Card className="h-100 border-opacity-25 project-card">
+              <div className="position-relative">
+                <Badge className="position-absolute top-0 end-0 m-2">
+                  Case Study
+                </Badge>
+              </div>
               <Card.Body>
                 <Card.Title>Mobile Chat App</Card.Title>
                 <Card.Subtitle className="mb-3">
@@ -252,6 +258,13 @@ function Projects() {
                   target="_blank"
                 >
                   <BsGithub className="me-1" /> Code
+                </Button>
+                <Button
+                  className="outline-button"
+                  size="sm"
+                  onClick={handleShowMobileCaseStudy}
+                >
+                  <BsFileText className="me-1" /> Case Study
                 </Button>
               </Card.Footer>
             </Card>
@@ -342,7 +355,7 @@ function Projects() {
                   alt="myFlix React Client"
                   fluid
                   rounded
-                  className="mb-4 first-casestudy-picture"
+                  className="mb-4 casestudy-picture"
                 />
 
                 {/* ===== Team & Roles ============================================== */}
@@ -384,7 +397,7 @@ function Projects() {
                       alt="Screenshot of main view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                     <div>
@@ -415,7 +428,7 @@ function Projects() {
                       alt="Screenshot of movie view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                   </div>
@@ -427,7 +440,7 @@ function Projects() {
                       alt="Screenshot of profile view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                     <div>
@@ -464,7 +477,7 @@ function Projects() {
                       alt="Screenshot of login view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                   </div>
@@ -518,16 +531,16 @@ function Projects() {
                     alt="Screenshot of Add Favorite Button"
                     fluid
                     rounded
-                    className="me-3"
-                    style={{ maxWidth: "30%", height: "40px" }}
+                    className="me-3 casestudy-picture"
+                    style={{  height: "40px", width: "auto" }}
                   />
                   <Image
                     src="/images/RemoveFav.png"
                     alt="Screenshot of Remove Favorite Button"
                     fluid
                     rounded
-                    className="me-3"
-                    style={{ maxWidth: "30%", height: "40px" }}
+                    className="me-3 casestudy-picture"
+                    style={{ height: "40px", width: "auto" }}
                   />
 
                   <li>
@@ -587,7 +600,7 @@ function Projects() {
                   alt="myFlix React Client"
                   fluid
                   rounded
-                  className="mb-4 first-casestudy-picture"
+                  className="mb-4 casestudy-picture"
                 />
 
                 {/* ===== Team & Roles ============================================== */}
@@ -636,7 +649,7 @@ function Projects() {
                       alt="Screenshot of sign up view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                   </div>
@@ -649,7 +662,7 @@ function Projects() {
                       alt="Screenshot of a single movie card"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                     <div>
@@ -668,14 +681,14 @@ function Projects() {
                         alt="Screenshot of Add Favorite modal"
                         fluid
                         rounded
-                        className="me-3"
+                        className="me-3 casestudy-picture"
                         style={{ width: "100%", marginBottom: "1rem" }}
                       /><Image
                         src="/images/AngularRemoveFav.png"
                         alt="Screenshot of Remove Favorite modal"
                         fluid
                         rounded
-                        className="me-3"
+                        className="me-3 casestudy-picture"
                         style={{ width: "100%" }}
                       />
                     </div>
@@ -688,7 +701,7 @@ function Projects() {
                       alt="Screenshot of profile view"
                       fluid
                       rounded
-                      className="me-3"
+                      className="me-3 casestudy-picture"
                       style={{ width: "30%" }}
                     />
                     <div>
@@ -697,7 +710,7 @@ function Projects() {
                     </div>
                   </div>
 
-                {/* ===== UX & Responsive Work ====================================== */}
+                {/* Other Highlights */}
                 <h4 className="colored-text">
                   Other Highlights
                 </h4>
@@ -733,16 +746,16 @@ function Projects() {
                     alt="Screenshot of Add Favorite icon"
                     fluid
                     rounded
-                    className="me-3"
-                    style={{ maxWidth: "30%", height: "40px" }}
+                    className="me-3 casestudy-picture"
+                    style={{ maxWidth: "30%", height: "auto" }}
                   />
                   <Image
                     src="/images/FavIcon.png"
                     alt="Screenshot of Favorited movie icon"
                     fluid
                     rounded
-                    className="me-3"
-                    style={{ maxWidth: "30%", height: "40px" }}
+                    className="me-3 casestudy-picture"
+                    style={{ maxWidth: "30%", height: "auto" }}
                   />
 
                   <li>
@@ -766,6 +779,162 @@ function Projects() {
                 <BsBoxArrowUpRight className="me-2" /> View Live Project
               </Button>
             </Modal.Footer>
+          </Modal>
+        )}
+
+        {/* Mobile Chat App case study */}
+        {showCaseStudy === "mobile" && (
+          <Modal show={showCaseStudy} onHide={handleClose} size="lg">
+            <Modal.Header closeButton>
+              <Modal.Title>Mobile Chat App Case Study</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <>
+                {/* ===== Overview =================================================== */}
+                <h3 className="colored-text">Project Overview</h3>
+                <div className="d-flex align-items-start mb-4">
+                  <p className="mb-4">
+                    This project marked my introduction to mobile app development, Firebase, and real-time messaging. I developed a mobile chat application using React Native and Expo that enables users to send and receive messages instantly. The app also supports image and location sharing, with proper permission handling. Firebase powers the authentication and data storage, while the chat interface is built using GiftedChat.
+                  </p>
+                  <Image
+                    src="/images/Mobile-App.png"
+                    alt="Mobile Chat App"
+                    fluid
+                    rounded
+                    className="mb-4 casestudy-picture"
+                    style={{ width: "25%", height: "auto" }}
+                  />
+                </div>
+
+                {/* ===== Team & Roles ============================================== */}
+                <h5 className="colored-text">Team &amp; Mentorship</h5>
+                <ul className=" mb-4 ps-3">
+                  <li>Lead Developer – Hannah Hogan</li>
+                  <li>Mentor – Akunna Nwosu</li>
+                  <li>Tutor – Jubril Akolade</li>
+                  <li>Consultant – Tim Pamplin</li>
+                </ul>
+
+                {/* ===== Tools & Tech ============================================== */}
+                <h4>Tools &amp; Tech Stack</h4>
+                <div className="mb-3">
+                  {[
+                    { label: "React Native" },
+                    { label: "Firebase" },
+                    { label: "Expo" },
+                    { label: "Gifted Chat" },
+                    { label: "Action Sheet" },
+                    { label: "Image Picker" },
+                    { label: "Maps" },
+                  ].map(({ bg, label }) => (
+                    <Badge key={label} bg={bg} className="me-1 p-2">
+                      {label}
+                    </Badge>
+                  ))}
+                </div>
+
+                {/* Capabilities*/}
+                <h4>Capabilities</h4>
+                <div className="mb-4">
+
+                  {/* Background Color Picker */}
+                  <div className="d-flex align-items-start mb-4">
+                    <div>
+                      <strong className="colored-text">
+                        Background Color Picker: 
+                      </strong>
+                      I made use of React's <strong>useState</strong> hook to allow users to change the background color of the chat interface. This feature adds a personal touch to the app, allowing users to customize their experience.
+                    </div>
+                    <Image
+                      src="/images/BackgroundPicker.png"
+                      alt="Screenshot of background color picker"
+                      fluid
+                      rounded
+                      className="me-3 casestudy-picture"
+                      style={{ width: "30%"}}
+                    />
+                  </div>
+                </div>
+
+                  {/* Cached Messages with Firestore */}
+                  <div className="d-flex align-items-start mb-4">
+                    <div>
+                      <strong className="colored-text">Cached Messages:</strong> The chat screen lets users send and receive messages in real time using Firebase Firestore, while also saving recent messages on the device. When the user is online, new messages appear instantly and are saved locally in case the connection drops. If the user is offline, the app loads the most recent messages from the device so they can still read past conversations. This makes the chat experience fast, reliable, and available even without internet access.
+                    </div>
+                  </div>
+
+                  {/* Asking Permission */}
+                  <div className="d-flex align-items-start mb-4">
+                    <div>
+                      <strong className="colored-text">Asking Permission:</strong> The app asks for permission to access certain device features only when needed, keeping the experience intuitive and respectful of user privacy. For example, permission to access the photo library is requested only when a user taps the image upload button, and location access is requested only if the user chooses to share their location in a message. These prompts are built to be clear and contextual, following platform best practices to ensure users understand why a permission is being asked at the moment it's relevant.
+                    </div>
+                  </div>
+
+                  {/* Send pictures and location */}
+                  <div className="d-flex align-items-start mb-4" style={{ gap: "2rem" }}>
+                    <div className="d-flex flex-column" style={{ flex: 1 }}>
+                      <Image
+                        src="/images/Actions.png"
+                        alt="Screenshot of Possible Actions"
+                        fluid
+                        rounded
+                        className="me-3 casestudy-picture"
+                        style={{ width: "100%", marginBottom: "1rem" }}
+                      />
+                    </div>
+                    <div style={{ flex: 2 }}>
+                      <strong className="colored-text">Actions:</strong> Once permission has been granted, users can enhance their chats by sending photos directly from their device and optionally sharing their current location. These features make conversations more expressive and helpful—whether it’s sharing a picture from your day or letting a friend know where to meet. Both features are entirely optional and only activate when the user chooses to use them.
+                    </div>
+                  </div>
+
+
+                {/* ===== UX & Responsive Work ====================================== */}
+                <h4 className="colored-text">
+                  Other Highlights
+                </h4>
+                <ul className="mb-4">
+                  <li>
+                    The user can input their name at the beginning of the app, which is then used to identify them in the chat. This is a simple but effective way to personalize the chat experience.
+                  </li>
+                  <li>
+                    The user can choose whether to select a picture from their device or take a new one using the camera. This is done using the <strong>Image Picker</strong> library, which provides a simple interface for selecting images.
+                  </li>
+                </ul>
+
+                {/* ===== Development Journey ====================================== */}
+                <h4 className="colored-text">Development Journey</h4>
+                <p className="mb-3">
+                  Though it was simple, I enjoyed the challenge of building a mobile app from scratch. I had to learn how to use React Native and Expo, which was a new experience for me. I also had to learn how to use Firebase for data storage, which was a bit tricky at first, but I eventually got the hang of it. The biggest challenge was getting the chat interface to work properly, but I was able to use the GiftedChat library to make it easier.
+                </p>
+
+                {/* ===== Challenges & Solutions =================================== */}
+                <h5 className="colored-text">
+                  Notable Challenges & How I Solved Them
+                </h5>
+                <ul className="mb-4">
+                  <li>
+                    <strong>Second User:</strong> In order to test the app, I needed to have a second user to chat with. I made use of an android Emulator, since I have an iphone, allowing me to test both major types of phones. This allowed me to see how the app would behave on different devices and ensure that it was responsive and functional across platforms.
+                  </li>
+                  <Image
+                    src="/images/SecondUser.png"
+                    alt="Screenshot of Second User"
+                    fluid
+                    rounded
+                    className="me-3 casestudy-picture"
+                    style={{ maxWidth: "50%", height: "auto" }}
+                  />
+                  <li>
+                    <strong>Firebase Storage:</strong> I had to learn how to use Firebase Storage to store images and other media files. This was a bit tricky at first, but I was able to find some good documentation and examples online that helped me get started. I also had to learn how to use the Firebase Realtime Database to store messages, which was a bit different from what I was used to with traditional databases.
+                  </li>
+                </ul>
+
+                {/* ===== Outcome & Next Steps ===================================== */}
+                <h4 className="colored-text">Outcome & Next Steps</h4>
+                <p className="mb-2">
+                  All in all, this was a pretty simple app, but it was a great introduction to mobile app development. I learned a lot about how to build a mobile app from scratch, and I was able to apply some of the skills I had learned in my previous projects. I would like to add more features, such as push notifications and a way to filter messages by user or date. I would also like to improve the UI and make it more visually appealing.
+                </p>
+              </>
+            </Modal.Body>
           </Modal>
         )}
       </Container>
